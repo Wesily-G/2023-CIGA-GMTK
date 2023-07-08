a408785d1d1bbee5b12e4cff52635ee11b22c202
Wesily-G <729009583@qq.com>
2023-07-08 12:44:18 +0800

    boss逻辑，excel表更新，spell预留

---
+++ 2023-CIGA-GMTK/Assets/Mingbin/Spells/Fire/Spell_FireMinion.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Spells/Water/FireMinion")]
+public class Spell_FireMinion : Spells
+{
+    public override void OnCast(Monster monster, bool castedByMonster = false)
+    {
+        base.OnCast(monster, castedByMonster);
+        //Place Holder
+    }
+}
