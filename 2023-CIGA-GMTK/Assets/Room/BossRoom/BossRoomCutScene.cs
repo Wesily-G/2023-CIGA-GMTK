84467e119c68285e0561e444de46b1db85a18b95
leking502 <64837763+leking502@users.noreply.github.com>
2023-07-09 23:34:25 +0800

    剧情和绑定

--- 2023-CIGA-GMTK/Assets/Room/BossRoom/BossRoomCutScene.cs
+++ 2023-CIGA-GMTK/Assets/Room/BossRoom/BossRoomCutScene.cs
@@ -22,18 +22,31 @@ public class BossRoomCutScene : MonoBehaviour
         switch (name.Split("_")[2].Replace("(Clone)",""))
         {
             case "1":
-                print("dialogue");
-                DialogueManager.AddDialogue("你","你是谁？");
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"你是谁？");
                 DialogueManager.AddDialogue("？？？","你不能再往前进了？");
-                DialogueManager.AddDialogue("你","城堡里面究竟有什么？");
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"城堡里面究竟有什么？");
                 DialogueManager.AddDialogue("？？？","你不需要知道？");
                 DialogueManager.StartDialogue();
                 break;
             case "2":
+                DialogueManager.AddDialogue("？？？","你来到这里了吗。");
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"为什么不让我进去？");
+                DialogueManager.AddDialogue("？？？","哎，你会知道的。");
+                DialogueManager.StartDialogue();
                 break;
             case "3":
+                DialogueManager.AddDialogue("？？？","战胜我吧。");
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"。。。。。");
+                DialogueManager.StartDialogue();
                 break;
             case "4":
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"你是。。。");
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name+"???",$"我是{GameObject.FindWithTag("Player").GetComponent<Player>().name}");
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"你是。。我？？");
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name+"???","战胜我吧，不然别想再进去一步。");
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"为什么！");
+                DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name+"???","想成为我的话就战胜我！");
+                DialogueManager.StartDialogue();
                 break;
         }
     }
--- 2023-CIGA-GMTK/Assets/Room/EndingRoom/EndingRoom.cs
+++ 2023-CIGA-GMTK/Assets/Room/EndingRoom/EndingRoom.cs
@@ -17,6 +17,15 @@ public class EndingRoom : MonoBehaviour
 
     private void StartCutScene()
     {
+        DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"这里就是核心吗。");
+        DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"其实我知道了，我不是你是吗。");
+        DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"我才是外来者。");
+        DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"这是你的记忆。");
+        DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"是你引导我到这里来的吗。");
+        DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"原来如此，你希望我接管这具身体。");
+        DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,"我会的，一直以来幸苦了。");
+        DialogueManager.AddDialogue(GameObject.FindWithTag("Player").GetComponent<Player>().name,$"感谢你{GameObject.FindWithTag("Player").GetComponent<Player>().name}");
+
         DialogueManager.AddDialogue("Developers",$"感谢你的游玩{GameObject.FindWithTag("Player").GetComponent<Player>().name}");
         DialogueManager.AddDialogue("Developers","Programmming: Leking, soraku7, Blueberry, Resonance");
         DialogueManager.AddDialogue("Developers","Design: Resonance, Bluebarry, Leking");
